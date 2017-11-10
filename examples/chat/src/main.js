550e0620493bd2f8248e25329ddde933f47fbe3b
ray <rayckyau@github.com>
2017-11-09 20:44:43 -0500

    attempt to block pulltorefresh

--- examples/chat/src/main.js
+++ examples/chat/src/main.js
@@ -40,7 +40,52 @@ let HOSTNAME = process.env.HOSTNAME || 'localhost';
       return false;
     }
 
-    //onbeforeunload
+    //prevent pull to refresh
+
+    var preventPullToRefresh = (function preventPullToRefresh(lastTouchY) {
+      lastTouchY = lastTouchY || 0;
+      var maybePrevent = false;
+
+      function setTouchStartPoint(event) {
+        lastTouchY = event.touches[0].clientY;
+        // console.log('[setTouchStartPoint]TouchPoint is ' + lastTouchY);
+      }
+      function isScrollingUp(event) {
+        var touchY = event.touches[0].clientY,
+            touchYDelta = touchY - lastTouchY;
+
+        // console.log('[isScrollingUp]touchYDelta: ' + touchYDelta);
+        lastTouchY = touchY;
+
+        // if touchYDelta is positive -> scroll up
+        if(touchYDelta > 0){
+          return true;
+        }else{
+          return false;
+        }
+      }
+
+    return {
+      // set touch start point and check whether here is offset top 0
+      touchstartHandler: function(event) {
+        if(event.touches.length != 1) return;
+        setTouchStartPoint(event);
+        maybePrevent = window.pageYOffset === 0;
+        // console.log('[touchstartHandler]' + maybePrevent);
+      },
+      // reset maybePrevent frag and do prevent
+      touchmoveHandler: function(event) {
+        if(maybePrevent) {
+          maybePrevent = false;
+          if(isScrollingUp(event)) {
+            // console.log('======Done preventDefault!======');
+            event.preventDefault();
+            return;
+          }
+        }
+      }
+    }
+  })();
 
     let doc = $(document),
       drawcanvas = $('#paper'),
@@ -58,6 +103,8 @@ let HOSTNAME = process.env.HOSTNAME || 'localhost';
     let prev = {};
 
     /////////stuff to do when page loads/////////////
+    document.addEventListener('touchstart', preventPullToRefresh.touchstartHandler);
+    document.addEventListener('touchmove', preventPullToRefresh.touchmoveHandler);
     //check session
     checkSession();
     //init canvas bindings
