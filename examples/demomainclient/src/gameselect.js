41575cb4618ed8c46f800947357084d65488a96a
ray <rayckyau@github.com>
2017-12-13 19:01:32 -0500

    changed UI for game description

--- examples/demomainclient/src/gameselect.js
+++ examples/demomainclient/src/gameselect.js
@@ -3,7 +3,7 @@
 export class GameSelectScreen extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {gamedescription: "description", adminnum: 0, selection: "lobby"};
+    this.state = {gamename: "gamename", gamedescription: "description", playersallowed: "Players:", adminnum: 0, selection: "lobby"};
   }
 
   componentDidMount() {
@@ -33,27 +33,29 @@ export class GameSelectScreen extends React.Component {
     console.log("updateSelection" + $.retlastVote());
     if ($.retlastVote() != this.state.selection ){
       if ($.retlastVote() == 'Illuminati Imposter'){
-        let descrip = 'Welcome to Illuminati Imposter. In this game, you all ' +
+        let descrip = 'In this game, you all ' +
         'belong to the Illuminati except for one player who is the Imposter.' +
         ' All of the Illuminati will receive the secret word. On the main ' +
         'screen, 4 words will be shown but only one of them will be the ' +
         'secret word. Players will have two rounds to draw pictures related ' +
         'to the secret word. Players will then vote on the Imposter. If the '+
         'Imposter is found out, they will have a final chance to win by '+
-        'correctly choosing the secret word!'
-        this.setState({gamedescription: descrip, adminnum: this.state.adminnum, selection: "Illuminati Imposter"});
+        'correctly choosing the secret word!';
+        let players = 'Players: 4-8';
+        this.setState({gamename: "Illuminati Imposter", gamedescription: descrip, playersallowed: players, adminnum: this.state.adminnum, selection: "Illuminati Imposter"});
       }
       else if ($.retlastVote() == 'Assembly Line'){
-        let descrip = 'Welcome to Assembly Line. You belong to a team of ' +
-        ' robo-artists and your company manufactures paintings. Each member ' +
+        let descrip = 'You belong to a team of ' +
+        'robo-artists and your company manufactures paintings. Each member ' +
         'of your team will be drawing a part of the picture. Each robot will ' +
         'have a limited time to draw their part of the picture. The final ' +
         'robot manager will have to guess the correct word being drawn. The ' +
-        'more points the better!'
-        this.setState({gamedescription: descrip, adminnum: this.state.adminnum, selection: "Assembly Line"});
+        'more points the better!';
+        let players = 'Players: 2-10';
+        this.setState({gamename: "Assembly Line", gamedescription: descrip, playersallowed: players, adminnum: this.state.adminnum, selection: "Assembly Line"});
       }
       else{
-        this.setState({gamedescription: "Back To Lobby", adminnum: this.state.adminnum, selection: "lobby"})
+        this.setState({gamename: "", gamedescription: "Back To Lobby", playersallowed: "", adminnum: this.state.adminnum, selection: "lobby"});
       }
     }
   }
@@ -89,9 +91,13 @@ export class GameSelectScreen extends React.Component {
 
        </div>
        <div className="col-sm-6 gridcenter">
+        <div id="aligndescrip">
           <div id="gamethumbnail">
           </div>
+          <div id="gamename">{this.state.gamename}</div>
+          <div id="allowedplayers">{this.state.playersallowed}</div>
           <div id="gamedescription">{this.state.gamedescription}</div>
+        </div>
        </div>
        </div>
      </div>
@@ -110,31 +116,31 @@ class GameSelection extends React.Component {
       //make selection bold
       return (
         <ul>
-          <li><b>Illuminati Imposter (Beta)</b></li>
+          <li><b>Illuminati Imposter</b></li>
           <li>Assembly Line (In development)</li>
           <li>Lobby</li>
         </ul>
-      )
+      );
     }
     else if (this.props.selection == "Assembly Line"){
       //make selection bold
       return (
         <ul>
-          <li>Illuminati Imposter (Beta)</li>
+          <li>Illuminati Imposter</li>
           <li><b>Assembly Line (In development)</b></li>
           <li>Lobby</li>
         </ul>
-      )
+      );
     }
     else{
       //make selection bold
       return (
         <ul>
-          <li>Illuminati Imposter (Beta)</li>
+          <li>Illuminati Imposter</li>
           <li>Assembly Line (In development)</li>
           <li><b>Lobby</b></li>
         </ul>
-      )
+      );
     }
   }
 }
