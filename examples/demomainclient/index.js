e181f18257bb20788a19bffaada105ae7bcc02e2
ray <rayckyau@github.com>
2017-12-14 17:11:45 -0500

    added helmet

--- examples/demomainclient/index.js
+++ examples/demomainclient/index.js
@@ -1,5 +1,5 @@
-//var compression = require('compression');
 var express = require('express');
+var helmet = require('helmet');
 const fs = require('fs');
 var serverport = 3001;
 var expiryDate = new Date(Date.now() + 60 * 60 * 1000 * 10); //10 hour
@@ -9,6 +9,7 @@ var server  = require("http").createServer(app);
 
 //special reverse proxy headers settings
 app.set('trust proxy', 1);
+app.use(helmet());
 
 server.listen(3001, function () {
   console.log('mainclient listening at port %d', 3001);
